7ac5dc355b108d152be8880889e6e8772647e819
huzhijian <18360577010@163.com>
2021-05-17 18:17:29 +0800

    修改配置

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
+const ENV = process.env.NODE_ENV;
 module.exports = {
     devServer: {
         port: 9091,
         open: true
-    }
+    },
+    publicPath: ENV === "development" ? "" : "/drag-demo/"
 }
