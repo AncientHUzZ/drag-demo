5ccc960d673fe3fd8ad352733c92fc74b5c99a1e
huzhijian <18360577010@163.com>
2021-05-17 18:22:00 +0800

    删除

--- src/views/index.js
+++
@@ -1,9 +0,0 @@
-import Page from './page-component'
-import Config from './config-component'
-
-export default {
-	install(Vue) {
-		Vue.use(Page)
-		Vue.use(Config)
-	}
-}
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const ENV = process.env.NODE_ENV;
-module.exports = {
-    devServer: {
-        port: 9091,
-        open: true
-    },
-    publicPath: ENV === "development" ? "" : "/drag-demo/"
-}
